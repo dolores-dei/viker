fd6e787d41e1092f117b9937705b6b28cd8b74d2
Ivo Ruzicka <ivo.ruzicka@tutanota.com>
2024-01-24 15:25:42 +0100

    fix default video player

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 use clap::Parser;
 use std::env::consts::{OS, ARCH};
 
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
     anime_name: Option<String>,
-    #[arg(short, long, default_value_t = default_video_player())]
-    video_player: Option<String>
 
+    #[arg(short, long, default_value_t = default_video_player())]
+    video_player: String,
 }
+
 fn default_video_player() -> String {
     match (OS, ARCH) {
         ("macos", "aarch64") => "iina".to_string(), // Apple Silicon
         ("linux", "x86_64") => "vlc".to_string(),  // Linux AMD64
-        _ => "vlc".to_string(),
+        _ => "vlc".to_string(),                    // Default for other OS/Arch combinations
     }
 }
 
@@ -23,8 +24,6 @@ fn main() {
     if let Some(name) = cli.anime_name {
         println!("{}", name);
     }
-    if let Some(video) = cli.video_player {
-        println!("{}", video);
-    }
+    println!("{}", cli.video_player);
 
 }
