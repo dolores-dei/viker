2ef4c7c3934fdd8280efc5a9601cba9a2e2feb0d
Ivo Ruzicka <ivo.ruzicka@tutanota.com>
2024-02-02 13:35:13 +0100

    handle invalid response in provider init add tests

--- src/api.rs
+++ src/api.rs
@@ -195,16 +195,35 @@ pub async fn get_episode_url(
 }
 
 pub fn provider_init(resp: &str) -> Result<String, Box<dyn Error>> {
+    if resp.len() < 30 { return Err("non valid response: too small".into()); }
     let mut command = format!("printf \"%s\" {} | head -1 | cut -d':' -f2 | sed 's/../&\\n/g' | sed 's/^01$/9/g;s/^08$/0/g;s/^05$/=/g;s/^0a$/2/g;s/^0b$/3/g;s/^0c$/4/g;s/^07$/?/g;s/^00$/8/g;s/^5c$/d/g;s/^0f$/7/g;s/^5e$/f/g;s/^17$/\\//g;s/^54$/l/g;s/^09$/1/g;s/^48$/p/g;s/^4f$/w/g;s/^0e$/6/g;s/^5b$/c/g;s/^5d$/e/g;s/^0d$/5/g;s/^53$/k/g;s/^1e$/\\&/g;s/^5a$/b/g;s/^59$/a/g;s/^4a$/r/g;s/^4c$/t/g;s/^4e$/v/g;s/^57$/o/g;s/^51$/i/g;' | tr -d '\\n'", resp);
     command.replace("clock", "clock.json");
 
     let output = Command::new("sh").arg("-c").arg(&command).output()?;
 
-
     if !output.status.success() {
         return Err("Command execution failed".into());
     }
 
-    let output= String::from_utf8(output.stdout)?.trim().to_string().replace("clock", "clock.json");
+    let output = String::from_utf8(output.stdout)?
+        .trim()
+        .to_string()
+        .replace("clock", "clock.json");
     Ok(output)
+}
+
+#[test]
+fn test_provider_init_success() {
+    let resp = "175948514e4c4f57175b54575b5307515c050f5c0a0c0f0b0f0c0e590a0c0b5b0a0c0a010a010f080e5e0e0a0f0d0f0a0f0c0e0b0e0f0e5a0e5e0e000e090a000e5e0e010a010e590e010e0f0e0a0a000f0e0e5d0f0e0b010e5e0e0a0b5a0c5a0d0a0c5a0f5b0c010d0a0c0f0b0d0a080f0a0e5e0f0a0e590e0b0b5a0c0c0e010e5c0f0b0a5c0e000e010a5c0c5d0e0b0f0c0e010a5c0c0f0e0d0e0f0e0a0e0b0e5a0e5e0e0f0a5c0b0a0f0a0e5d0a5c0d0d0e0b0e0f0f0d0e010e000a080f0a0f5e0f0e0e0b0f0d0f0b0e0c0b5a0d0d0d0b0c0c0a080f0d0f0b0e0c0b5a0a080e0d0e010f080e0b0f0c0b5a0d5e0b0c0b5e0b0c0d5b0d5d0c5e0f080d5e0e5a0b5e0f0c0e0a0d0d0b0f0f0b0e0c0f5e0b0f0e010d5b0d5d0c5b0f080c590d090c080e5b0d5e0d090d0c0e590e0c0d090e590e5d0c590d0a0d0c0b0e0e0f0c0d0b0f0f5b0d5b0d090c080f5b0e0c0b0c0b0a0f0b0e0d0c090b0b0e000a0c0a590a0c0f0d0f0a0f0c0e0b0e0f0e5a0e0b0f0c0c5e0e0a0a0c0b5b0a0c0f080e5e0e0a0e0b0e010f0d0f0a0f0c0e0b0e0f0e5a0e5e0e010a0c0a590a0c0e0a0e0f0f0a0e0b0a0c0b5b0a0c0b0c0b0e0b0c0b0a0a5a0b0e0b0f0a5a0b0c0b080d0a0b0f0b0d0b5b0b0e0b0d0b5b0b0e0b0e0a000b0e0b0e0b0e0d5b0a0c0f5a1e4a5d5e5d4a5d4a05";
+    let result = provider_init(resp).unwrap();
+    println!("{:?}", result);
+    assert!(result.contains("clock.json"));
+}
+
+#[test]
+fn test_provider_init_error() {
+    let resp = "invalid response";
+    let result = provider_init(resp);
+
+    assert!(result.is_err());
 }
